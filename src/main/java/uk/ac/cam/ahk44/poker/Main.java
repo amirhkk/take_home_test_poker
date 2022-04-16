6d99dc6421c3a803cc8f0dc165bdc6ddbf2b147c
amirhkk <amir.hkkh@gmail.com>
2022-04-16 17:18:37 +0100

    No testing yet.

--- src/main/java/uk/ac/cam/ahk44/poker/Main.java
+++ src/main/java/uk/ac/cam/ahk44/poker/Main.java
@@ -25,7 +25,7 @@ public class Main {
 
     Deck deck = Deck.create();
     List<Count> counts = Simulator.simulate(deck, 500000);
-    for (Count c : counts) {
+    for (Count c : counts){
       System.out.printf("%20s %2.2e %2.2e %n", c.getHandRank(), c.getDealProbability().doubleValue(),
               c.getWinProbability().doubleValue());
     }
